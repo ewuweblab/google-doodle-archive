b5843955cb6988ec60aa9465abf229c7a0689e16
ewuweblab <pmanikoth@ewu.edu>
2017-02-22 13:44:55 -0800

    Randomized Image
    * Random homepage images
    * JS to randomize, not CSS

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
-// YOUR SCRIPTS GO HERE
+// Store 3 images
+var img0 = '../img/200.jpg';
+var img1 = '../img/300.jpg';
+var img2 = '../img/400.jpg';
+
+// Get random number
+var max = 3;
+var numRand = Math.floor( Math.random() * max );
+console.log(numRand);
+
+// Display randomized images
+$('img').attr( 'src', eval('img' + numRand) );
+
